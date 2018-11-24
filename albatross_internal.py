ab7e7df5fa1f0daef19e3258032fb1a6402c1738
vishalkuo <vishalkuo@gmail.com>
2018-11-23 20:51:38 -0800

    Adds image creation flow

--- albatross_internal.py
+++ albatross_internal.py
@@ -14,25 +14,59 @@ SLACK_WEBHOOK = os.environ.get("SLACK_WEBHOOK")
 
 def handle(event, context):
     client = boto3.client("ec2")
-    server = _find_devserver(client)
-
     logger.warn(f"Received event {event}")
+
+    server = _find_devserver(client)
     if not server:
         return {"statusCode": 400, "body": "Server not found"}
+    if event["detail-type"] == "EC2 Instance State-change Notification":
+        return _process_instance_state_change(client, event, server)
+    if event["detail-type"] == "Scheduled Event":
+        return _process_cron(client, event, server)
 
+    return {"statusCode": 200, "body": "noop"}
+
+
+def _process_cron(client, event, server):
+    images = client.describe_images(
+        Filters=[{"Name": "tag:application", "Values": [DEVSERVER]}]
+    )
+    if not images["Images"]:
+        return {"statusCode": 200, "body": "noop"}
+
+    image = images["Images"][0]
+    if image["State"] == "available":
+        slack_str = "Successfully terminated image"
+        try:
+            client.terminate_instances(InstanceIds=[server["InstanceId"]])
+        except Exception as e:
+            slack_str = f"Couldn't terminate instance: {e}"
+        requests.post(SLACK_WEBHOOK, json={"text": slack_str})
+        return {"statusCode": 200, "body": "Terminated instance"}
+
+    return {"statusCode": 200, "body": "noop"}
+
+
+def _process_instance_state_change(client, event, server):
     if server["InstanceId"] != event["detail"]["instance-id"]:
         return {"statusCode": 400, "body": "Irrelevant instance id"}
 
-    slack_str = ""
-    state = event["detail"]["state"]
-    if state == "stopped":
-        slack_str = "Instance has stopped, terminating..."
-        client.terminate_instances(InstanceIds=[server["InstanceId"]])
-    else:
-        slack_str = "Instance terminated"
+    if event["detail"]["state"] != "stopped":
+        return {"statusCode": 400, "body": "Irrelevant state"}
+
+    instance_id = server["InstanceId"]
+    slack_str = "Instance has stopped, image created"
+    try:
+        res = client.create_image(InstanceId=instance_id, Name="albatross")
+        client.create_tags(
+            Resources=[res["ImageId"]],
+            Tags=[{"Key": "application", "Value": DEVSERVER}],
+        )
+    except Exception as e:
+        slack_str = f"Couldn't create image on shut down: `{e}`"
 
     requests.post(SLACK_WEBHOOK, json={"text": slack_str})
-    return {"statusCode": 200, "body": json.dumps(state)}
+    return {"statusCode": 200, "body": json.dumps("Creating snapshot")}
 
 
 def _find_devserver(client) -> Optional[any]:
--- export.sh
+++ export.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied, exiting..."
-    exit 1
-fi
-
-TO_EXPORT=$1
-echo "exporting $TO_EXPORT"
+echo "exporting..."
 
 
 cd env/lib/python3.7/site-packages/
-zip -r9 export.zip .
+zip -r9 -q export.zip .
 mv export.zip ../../../../
 cd ../../../../
-zip -g export.zip $TO_EXPORT
-
+zip -g export.zip *.py
