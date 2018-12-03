e723b2ab5d6a3116055019cac606f10f011dc0a3
vishalkuo <vishalkuo@gmail.com>
2018-12-02 18:29:43 -0800

    Adds tests

--- test/test_albatross_internal.py
+++ test/test_albatross_internal.py
@@ -10,7 +10,14 @@ def get_test_sns(message: str):
 
 
 def get_test_cron():
-    pass
+    return {"detail-type": "Scheduled Event"}
+
+
+def get_state_change(instance_id: str, state: str):
+    return {
+        "detail-type": "EC2 Instance State-change Notification",
+        "detail": {"instance-id": instance_id, "state": state},
+    }
 
 
 @patch("albatross_internal.aws")
@@ -79,7 +86,7 @@ class TestAlbatross(unittest.TestCase):
     ```"""
         )
 
-    def test_processess_sns_no_image(self, slack, boto3, constants, aws):
+    def test_processess_sns_with_image(self, slack, boto3, constants, aws):
         constants.UP = "up"
         resource = MagicMock()
         boto3.resource.return_value = resource
@@ -100,5 +107,131 @@ class TestAlbatross(unittest.TestCase):
     ```
     mosh -I albatross ec2-user@foobar.com
     ```"""
+        )
+
+    def test_processess_cron_with_running_server(self, slack, boto3, constants, aws):
+        client = MagicMock()
+        boto3.client.return_value = client
+        server = helper.get_mock_server(state="running")
+        aws.find_devserver.return_value = server
+
+        resp = albatross_internal.handle(get_test_cron(), None)
+
+        self.assertEqual(200, resp["statusCode"])
+        client.terminate_instances.assert_not_called()
+        slack.post.assert_not_called()
+
+    def test_processess_cron_with_unavailable_image(self, slack, boto3, constants, aws):
+        client = MagicMock()
+        boto3.client.return_value = client
+        server = helper.get_mock_server(state="stopped")
+        aws.find_devserver.return_value = server
+        aws.get_images.return_value = {"Images": [{"State": "pending"}]}
+
+        resp = albatross_internal.handle(get_test_cron(), None)
+
+        self.assertEqual(200, resp["statusCode"])
+        client.terminate_instances.assert_not_called()
+        slack.post.assert_not_called()
+
+    def test_processess_cron_with_available_image_no_deletion(
+        self, slack, boto3, constants, aws
+    ):
+        client = MagicMock()
+        boto3.client.return_value = client
+        server = helper.get_mock_server(state="stopped")
+        aws.find_devserver.return_value = server
+        aws.get_images.return_value = {"Images": [{"State": "available"}]}
+        client.describe_images.return_value = {"Images": []}
+
+        resp = albatross_internal.handle(get_test_cron(), None)
+
+        self.assertEqual(200, resp["statusCode"])
+        client.terminate_instances.assert_called_with(
+            InstanceIds=[server["InstanceId"]]
+        )
+        slack.post.assert_called_with("Image created and instance terminated")
+        client.deregister_image.assert_not_called()
+        client.delete_snapshot.assert_not_called()
+
+    def test_processess_cron_with_available_image_with_deletion(
+        self, slack, boto3, constants, aws
+    ):
+        client = MagicMock()
+        boto3.client.return_value = client
+        server = helper.get_mock_server(state="stopped")
+        aws.find_devserver.return_value = server
+        aws.get_images.return_value = {"Images": [{"State": "available"}]}
+        client.describe_images.return_value = {
+            "Images": [
+                {
+                    "ImageId": "foo",
+                    "BlockDeviceMappings": [{"Ebs": {"SnapshotId": "bar"}}],
+                }
+            ]
+        }
+
+        resp = albatross_internal.handle(get_test_cron(), None)
+
+        self.assertEqual(200, resp["statusCode"])
+        client.terminate_instances.assert_called_with(
+            InstanceIds=[server["InstanceId"]]
+        )
+        slack.post.assert_called_with("Image created and instance terminated")
+        client.deregister_image.assert_called_with(ImageId="foo")
+        client.delete_snapshot.assert_called_with(SnapshotId="bar")
+
+    def test_processess_state_change_for_irrelevant_server(
+        self, slack, boto3, constants, aws
+    ):
+        client = MagicMock()
+        boto3.client.return_value = client
+        server = helper.get_mock_server(state="stopped")
+        aws.find_devserver.return_value = server
+
+        resp = albatross_internal.handle(get_state_change("foo", "bar"), None)
+
+        self.assertEqual(200, resp["statusCode"])
+        slack.post.assert_not_called()
+        aws.create_image.assert_not_called()
+        client.create_tags.assert_not_called()
+
+    def test_processess_state_change_for_irrelevant_state(
+        self, slack, boto3, constants, aws
+    ):
+        client = MagicMock()
+        boto3.client.return_value = client
+        server = helper.get_mock_server(state="stopped")
+        aws.find_devserver.return_value = server
+
+        resp = albatross_internal.handle(
+            get_state_change(server["InstanceId"], "running"), None
+        )
+
+        self.assertEqual(200, resp["statusCode"])
+        slack.post.assert_not_called()
+        aws.create_image.assert_not_called()
+        client.create_tags.assert_not_called()
+
+    def test_processess_state_change_for_relevant_state_and_server(
+        self, slack, boto3, constants, aws
+    ):
+        client = MagicMock()
+        boto3.client.return_value = client
+        server = helper.get_mock_server(state="stopped")
+        aws.find_devserver.return_value = server
+        aws.get_images.return_value = {"Images": [{"ImageId": "foo"}]}
+        aws.create_image.return_value = {"ImageId": "id2"}
+
+        resp = albatross_internal.handle(
+            get_state_change(server["InstanceId"], "stopped"), None
+        )
+
+        self.assertEqual(200, resp["statusCode"])
+        slack.post.assert_called_with("Instance has stopped, image created")
+        aws.create_image.assert_called_with(client, server["InstanceId"])
+        client.create_tags.assert_called_with(
+            Resources=["id2"],
+            Tags=[{"Key": "application", "Value": constants.DEVSERVER}],
         )
 
